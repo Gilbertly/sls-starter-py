1558fdc499f102fd7f8273c9a19f679d1683ce3a
Gilbertly <gilbert@qubitping.com>
2019-10-07 19:05:38 +0300

    chore: add simple function with sentry integration

---
+++ aws-python/src/functions/hello.py
@@ -0,0 +1,13 @@
+import sentry_sdk
+from os import environ
+from sentry_sdk import capture_exception
+
+sentry_sdk.init(dsn=environ.get("SENTRY_DSN"))
+
+def handler(event, context):
+  try:
+    event_param = event["some_param"]
+  except Exception as error:
+    capture_exception(error)
+
+  return f"Event param: '{event_param}'"
