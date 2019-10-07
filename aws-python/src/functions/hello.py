6d4c0e47b99c70cbc93882d83ff127062cd97963
Gilbertly <gilbert@qubitping.com>
2019-10-07 23:45:48 +0300

    refactor: use pep8 best practices

--- aws-python/src/functions/hello.py
+++ aws-python/src/functions/hello.py
@@ -2,11 +2,12 @@ import sentry_sdk
 from os import environ
 from sentry_sdk import capture_exception
 
-sentry_sdk.init(dsn=environ.get("SENTRY_DSN"))
+sentry_sdk.init(dsn=environ.get('SENTRY_DSN'))
+
 
 def handler(event, context):
   try:
-    event_param = event["some_param"]
+    event_param = event['some_param']
   except Exception as error:
     capture_exception(error)
 
